85a68cc6dbe1ed3b972088695a2eaa839fde2543
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-01-16 18:07:35 -0500

    Update geo.js

--- src/js/views/geo.js
+++ src/js/views/geo.js
@@ -1,4 +1,14 @@
-import "../components/map.js";
+let testing = 'testing!';
+var map = null;
+var myOptions = {
+	
+		center: new google.maps.LatLng(40.600, -73.9120),
+		zoom: 13,
+		scrollwheel: false,
+		disableDefaultUI: true,
+	};
+  //map = new google.maps.Map(document.getElementById("map"), myOptions);
+
 
 export default () => /*html*/`
     <h1>Map</h1>
